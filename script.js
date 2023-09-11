a86311f610a8d0e4eb51257ef05c3caf87bf5070
Mikek044 <Michael.klski@gmail.com>
2023-09-11 16:42:18 +0200

    Finished Portfolio Website

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links")
+    const icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+    // targets an element and opens it class and adds styling
+}
